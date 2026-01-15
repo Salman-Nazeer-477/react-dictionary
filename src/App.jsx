44e59a9f74dfda3ef980245634bb00fbcdb324ea
Salman Nazeer <salmannazeer477@gmail.com>
2026-01-15 19:16:03 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -50,7 +50,7 @@ function App() {
         />
         <button>search</button>
       </form>
-      <div><h3>History:</h3>{wordHistory.map(word => <p onClick={() => setText(word)}>{word}<b onClick={() => deleteWordHistory(word)}>X</b></p>)}</div>
+      <div><h3>History:</h3>{wordHistory.map(word => <p onClick={() => setText(word)}>{word}<b onClick={() => deleteWordHistory(word)}> X</b></p>)}</div>
       {!result.title && result.map(word => <Word word={word} />)}
       {result.title && <h3>No words found!</h3>}
     </>
